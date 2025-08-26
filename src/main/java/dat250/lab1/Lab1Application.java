2e6ae47222fb707e2acf8a1702b5cb00be91f716
Isak <isyau8632@uib.no>
2025-08-26 16:17:36 +0200

    add text to the website

--- src/main/java/dat250/lab1/Lab1Application.java
+++ src/main/java/dat250/lab1/Lab1Application.java
@@ -2,12 +2,18 @@ package dat250.lab1;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
+@RestController
 public class Lab1Application {
 
 	public static void main(String[] args) {
 		SpringApplication.run(Lab1Application.class, args);
 	}
-
+	@GetMapping("/")
+	public String mainPage(){
+        return "HELLO World! <br> This is my submission for lab1";
+	}
 }
